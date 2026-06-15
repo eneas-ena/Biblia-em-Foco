1045edb08179f46c9c9d8fc4ff9c78769fb61554
eneas-ena <eneaspr@gmail.com>
2026-06-14 23:51:13 -0300

    Delete data directory

--- data/app-data.js
+++
@@ -1,5 +0,0 @@
-globalThis.appData = {
-  source: "Texto-base: João Ferreira de Almeida em domínio público via bible-api.com. Novo Testamento completo com marcações Strong automáticas; os evangelhos receberam reforço manual progressivo.",
-  books: [],
-  lexicon: {}
-};
