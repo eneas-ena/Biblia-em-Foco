b1edd0ce110e2bbae6b3e00a54889847cacf6679
eneas-ena <eneaspr@gmail.com>
2026-06-15 00:40:46 -0300

    Add files via upload

---
+++ data/app-data.js
@@ -0,0 +1,5 @@
+globalThis.appData = {
+  source: "Texto-base: ACF via thiagobodruk/biblia, licença CC BY-NC no repositório e direitos das traduções atribuídos aos respectivos autores. Bíblia completa; AT com primeira camada Strong hebraica, NT com Strong grego e evangelhos reforçados manualmente.",
+  books: [],
+  lexicon: {}
+};
