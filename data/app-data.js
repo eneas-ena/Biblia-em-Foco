81066b1693cfff8997c9d71fe1b5576d07510338
eneas-ena <eneaspr@gmail.com>
2026-06-15 21:53:23 -0300

    Delete data directory

--- data/app-data.js
+++
@@ -1,5 +0,0 @@
-globalThis.appData = {
-  source: "Texto-base: ACF via thiagobodruk/biblia, licença CC BY-NC no repositório e direitos das traduções atribuídos aos respectivos autores. Bíblia completa; AT com camada Strong hebraica ampliada e Gênesis lapidado, NT com Strong grego e evangelhos reforçados manualmente.",
-  books: [],
-  lexicon: {}
-};
